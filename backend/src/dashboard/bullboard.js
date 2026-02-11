85bb207576a3bb23247f152871d538cb454e3123
Jaykishan  Solanki <jaysolanki1733@gmail.com>
2026-02-11 14:59:44 +0530

    add linkedin Quque in the BullBoard

--- backend/src/dashboard/bullboard.js
+++ backend/src/dashboard/bullboard.js
@@ -4,6 +4,7 @@ import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 
 import { aiQueue } from '../queue/ai.queue.js';
 import { fetcherQueue } from '../queue/fetcher.queue.js';
+import { linkedinQueue } from '../queue/linkedin.queue.js'
 
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath('/admin/queues');
@@ -12,6 +13,7 @@ createBullBoard({
   queues: [
     new BullMQAdapter(aiQueue, { readOnlyMode: false }),
     new BullMQAdapter(fetcherQueue, { readOnlyMode: false }),
+    new BullMQAdapter(linkedinQueue, {readOnlyMode: false}),
   ],
   serverAdapter,
 });
